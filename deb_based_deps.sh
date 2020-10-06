307474cf5588d0ad3db548f6546903b3b1e0e558
earnestma <earnestma@protonmail.com>
2020-10-06 13:20:18 -0400

    Remove manual deploy, remove public submodule

--- deb_based_deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# For debian-based systems
-# Fail if error
-set -e
-wget -O /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.76.0/hugo_extended_0.76.0_Linux-64bit.deb
-sudo dpkg -i /tmp/hugo.deb
-rm /tmp/hugo.deb
