815d5f3f7ff43b8d85e030386d789d07adc48553
earnestma <earnestma@protonmail.com>
2020-10-06 11:55:10 -0400

    Bump Hugo version

--- deb_based_deps.sh
+++ deb_based_deps.sh
@@ -2,6 +2,6 @@
 # For debian-based systems
 # Fail if error
 set -e
-wget -O /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.75.1/hugo_extended_0.75.1_Linux-64bit.deb
+wget -O /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.76.0/hugo_extended_0.76.0_Linux-64bit.deb
 sudo dpkg -i /tmp/hugo.deb
 rm /tmp/hugo.deb
